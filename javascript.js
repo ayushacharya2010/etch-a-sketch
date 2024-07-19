707b84d75e1e41589f14a4a6b8e80958b5b4e6af
ayushacharya2010 <ayushbibo@gmail.com>
2024-07-19 10:47:45 -0500

    Complete grid ui and add prompt for # of squares

--- javascript.js
+++ javascript.js
@@ -4,9 +4,14 @@ document.addEventListener("DOMContentLoaded", () => {
     let i = 0;
 
     let row; // Declare row outside the loop to maintain scope
+    let number = parseInt(prompt("How many squares would you like on each side? Keep the number below 100."));
+    if (isNaN(number) || number < 1 || number > 100) {
+        alert("Invalid input. Please enter a number between 1 and 100.");
+        return;
+    }
 
     do {
-        if (i % 17 === 0) {
+        if (i % (number + 1) === 0) {
             row = document.createElement("div");
             row.classList.add("row");
             container.appendChild(row);
@@ -14,10 +19,17 @@ document.addEventListener("DOMContentLoaded", () => {
             const column = document.createElement("div");
             column.classList.add("column");
             row.appendChild(column);  
+
+            column.addEventListener('mouseover', () => {
+                column.style.backgroundColor = 'red';
+            })
         }
         i++;
-        if (i > 272) {
+        if (i > (number * (number + 1))) {
             keepGoing = false;
         }
     } while (keepGoing);
+
+
 });
+
